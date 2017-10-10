fadb441568ca47c84ff92484c977418ab4f84445
殷腊梅 <1141485216@qq.com>
2017-10-10 14:47:51 +0800

    委托学习

--- CLRViaCsharpStudy/DelegateTest/Program.cs
+++ CLRViaCsharpStudy/DelegateTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace DelegateTest
 {
@@ -6,7 +7,30 @@ namespace DelegateTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(obj => Console.WriteLine(obj), i);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(WriteData), i);
+                new Action<int>(WriteData2)(i);
+               Console.WriteLine(  new Func<int,string>(WriteData3)(i));
+            }
+
+                Console.ReadLine();
+        }
+
+
+        public static void  WriteData(object i)
+        {
+            Console.WriteLine(i);
+        }
+        public static void WriteData2(int i)
+        {
+            Console.WriteLine(i);
+        }
+        public static string   WriteData3(int i)
+        {
+            Console.WriteLine(i);
+            return "Func";
         }
     }
 }
