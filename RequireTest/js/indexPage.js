4eb7bfa219d5d8bd72d93995ca75ee140c287dd6
fineex.com <fineex.com@DESKTOP-QIL76U7>
2017-12-27 13:11:54 +0800

    vue练习

---
+++ RequireTest/js/indexPage.js
@@ -0,0 +1,49 @@
+﻿define(['vue'], function (vue) {
+    // 定义名为 todo-item 的新组件
+    vue.component('todo-item', {
+        // todo-item 组件现在接受一个
+        // "props"，类似于一个自定义特性。
+        // 这个 props 名为 todo。
+        //注意此处props是特定的参数不可以随便更改
+        props: ['todo'],
+        template: '<li>{{ todo.text }}</li>'
+    })
+    var data = { counter: 0 };
+
+    vue.component('simple-counter', {
+        template: '<button v-on:click="counter += 1">{{ counter }}</button>',
+        // 技术上 data 的确是一个函数了，因此 Vue 不会警告，
+        // 但是我们却给每个组件实例返回了同一个对象的引用
+        data: function () {
+            return { counter: 0 };
+        }
+    })
+
+    var appData = new vue({
+        el: "#app",
+        data: {  
+            isActive: true,
+            hasError: false,
+            checkedNames: [],
+            picked: '',
+            selected: '',
+            selectedc: [],
+            groceryList: [
+                { id: 0, text: '蔬菜' },
+                { id: 1, text: '奶酪' },
+                { id: 2, text: '随便其它什么人吃的东西' }
+            ]
+        },
+        computed: 
+        {
+            selectdestr:function ()
+            {
+                return "123ert789opt";
+            }
+        }
+    });
+
+
+
+    return appData;
+})
---
+++ RequireTest/js/initial.js
@@ -0,0 +1,22 @@
+﻿require.config({
+    //baseUrl: "js/lib",  整体目录是
+    paths: {
+        'jquery': 'http://apps.bdimg.com/libs/jquery/2.1.4/jquery',
+        'vue': 'http://apps.bdimg.com/libs/vue/1.0.8/vue',
+        //'goodsinfodetailPage': 'page/goods/infodetailPage',对外infodetailPage的名称是goodsinfodetailPage
+        //譬如在HTML页面上  data-page="themePage" 
+        'index':'indexPage'
+    }
+})
+
+require(['vue', 'jquery','index'],
+    function (Vue, jQuery,index) {
+        //alert("hello require");
+    })
+
+//测试用
+window.ajaxServer = "http://xingdian.fineex.cn";
+if (location.href.indexOf("localhost") >= 0 || location.href.indexOf("192.168.") >= 0) {
+    window.ajaxServer = "http://103.6.223.203:8008";
+    //window.ajaxServer = "http://vshopapi.fuqiantonglu.com";
+}
